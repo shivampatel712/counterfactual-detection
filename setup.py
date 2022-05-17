bcfa2e3575d8eedffa9761cd093d1933c35163ee
Shivam Patel <shivambpatel712@gmail.com>
2022-05-17 11:56:41 -0700

    added boilerplate code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Ou r take  on the counterfactual recognition task provided by SemEval 2020 Task 5.',
+    author='Shivam Patel',
+    license='MIT',
+)
